9844750f54f87d9596ce9cb60f2d664486592dd4
Moonhyuk Choi <dotnetpower@hotmail.com>
2018-01-08 17:08:05 +0900

    Add project files.

---
+++ GremlinConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GremlinConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
